791f17fdd83a4807d9a5badc9ddf86addfe5dafe
i9-9 <yvan.vrs@gmail.com>
2024-11-02 21:33:29 -0300

    added logo on hq for social media share

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,44 @@
-
-import Footer from '../components/Footer'
-import './globals.css'
-import { Analytics } from "@vercel/analytics/react"
+import Footer from '../components/Footer';
+import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Ivan Nevares',
   description: 'Web developer and UI designer interested in generating creative and performant digital experiences',
   icons: {
-    icon: {url: "/favicon_v2.ico"},
-  }
-}
+    icon: { url: '/favicon_v2.ico' },
+  },
+  openGraph: {
+    title: 'Ivan Nevares',
+    description: 'Web developer and UI designer interested in generating creative and performant digital experiences',
+    url: 'https://tu-sitio.com',
+    images: [
+      {
+        url: '/logo_rrss.png',
+        width: 1200,
+        height: 630,
+        alt: 'Imagen de Ivan Nevares',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Ivan Nevares',
+    description: 'Web developer and UI designer interested in generating creative and performant digital experiences',
+    image: ['/logo_rrss.png'],
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
-      <body className='custom'>
+      <body className="custom">
         {children}
         <Analytics />
         <Footer />
       </body>
     </html>
-  )
+  );
 }
