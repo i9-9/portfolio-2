9e869daa4b40baeb38b5beef24a6bb9a4603cc5b
i9-9 <yvan.vrs@gmail.com>
2025-01-22 20:46:22 -0300

    browser window component

--- app/page.tsx
+++ app/page.tsx
@@ -8,25 +8,54 @@ import Link from "next/link";
 
 const currentYear = new Date().getFullYear();
 
+const EmbeddedVideo = ({ videoId, className }) => {
+  if (!videoId) return null;
+
+  const videoUrl = `https://player.vimeo.com/video/${videoId}?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&loop=1&controls=0`;
+
+  return (
+    <div className={`relative ${className}`} style={{ paddingBottom: "56.25%", height: 0 }}>
+      <iframe
+        src={videoUrl}
+        frameBorder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+        title="Project Video"
+      ></iframe>
+    </div>
+  );
+};
+
 const ProfileLayout = () => {
   const projects = [
     {
       title: "El Desenfreno",
-      description: "Website for poetry publishing company from Buenos Aires, Argentina",
+      description:
+        "Website for poetry publishing company from Buenos Aires, Argentina",
       image: "/projects-eldesenfreno/eight.png",
       anchor: "https://eldesenfreno.com",
+      videoId: "",
     },
     {
       title: "Kostüme",
-      description: "Website/E-Commerce showcasing their minimalist fashion collections and unique design approach.",
+      description:
+        "Website/E-Commerce showcasing their minimalist fashion collections and unique design approach.",
       image: "/projects-landing/kostume/presentation.png",
       anchor: "https://kostumeweb.net",
+      videoId: "1049427357",
     },
     {
       title: "Newtro",
       description: "Website for web3 artist community",
       image: "/path/to/project3-image.jpg",
       anchor: "https://newtro.xyz",
+      videoId: "",
     },
   ];
 
@@ -63,19 +92,29 @@ const ProfileLayout = () => {
           <div className="flex flex-col mt-6 text-sm">
             <p className="text-lima">ivannevares9@gmail.com</p>
             <div className="text-sm text-lima mb-2">
-              <a href="https://dribbble.com/i9i9" target="_blank" className="hover:text-bluer">
+              <a
+                href="https://dribbble.com/i9i9"
+                target="_blank"
+                className="hover:text-bluer"
+              >
                 Dribbble
               </a>
               <span className="mr-1">,</span>
-              <a href="https://github.com/i9-9" target="_blank" className="hover:text-bluer">
+              <a
+                href="https://github.com/i9-9"
+                target="_blank"
+                className="hover:text-bluer"
+              >
                 Github
               </a>
             </div>
             <p className="text-sm mb-4 font-helveticaNowTextRegular text-mid-gray">
-              Designer with experience in websites and applications, dedicated to crafting unique, modern, and tailored experiences.
+              Designer with experience in websites and applications, dedicated
+              to crafting unique, modern, and tailored experiences.
             </p>
             <p className="text-sm mb-2 font-helveticaNowTextRegular text-mid-gray">
-              Currently studying Graphic Design at the Universidad de Buenos Aires.
+              Currently studying Graphic Design at the Universidad de Buenos
+              Aires.
             </p>
             <h1 className="text-sm mb-2 font-helveticaNowTextRegular text-mid-gray">
               Sound Technician, trained at TECSON, Buenos Aires.
@@ -110,25 +149,40 @@ const ProfileLayout = () => {
           </div>
         </div>
 
-        <div className="w-full md:w-3/4 p-6 flex flex-col justify-between items-start text-left rounded-xl">
+        <div className="w-full md:w-3/4 p-6 flex flex-col justify-between items-start text-left rounded-xl relative">
           {!isSlideshowActive ? (
             <h2 className="text-4xl md:text-8xl font-helveticaNowDisplayBlack mb-6 text-mid-gray">
-              An <span className="text-lima">experimental</span> designer with a focus on visual identity and digital design.
+              An <span className="text-lima">experimental</span> designer with a
+              focus on visual identity and digital design.
             </h2>
           ) : (
             <motion.div
-              className="flex flex-col p-4 rounded-lg bg-black w-full"
+              className="flex flex-col p-4 rounded-lg w-full relative"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               transition={{ duration: 0.5 }}
             >
-              <h2 className="text-4xl md:text-8xl font-helveticaNowDisplayBlack text-mid-gray">
-                {projects[currentProjectIndex].title}
-              </h2>
-              <p className="text-md text-light-gray text-left">
-                {projects[currentProjectIndex].description}
-              </p>
+              {projects[currentProjectIndex].videoId && (
+                <div className="relative w-full flex justify-center items-center">
+                  {/* Wrapper for browser window and video container */}
+                  <div className="relative w-full h-auto pb-[56.25%] bg-[#070707] rounded-lg overflow-hidden">
+                    {/* Browser top part */}
+                    <div className="h-8 bg-gray-500 rounded-t-lg flex items-center justify-start px-2 space-x-2">
+                      <div className="w-3 h-3 bg-gray-700 rounded-full"></div>
+                      <div className="w-3 h-3 bg-gray-700 rounded-full"></div>
+                      <div className="w-3 h-3 bg-gray-700 rounded-full"></div>
+                    </div>
+
+                    {/* Video container */}
+                    <div className="absolute top-8 left-0 w-full h-full">
+                      <EmbeddedVideo
+                        videoId={projects[currentProjectIndex].videoId}
+                      />
+                    </div>
+                  </div>
+                </div>
+              )}
             </motion.div>
           )}
         </div>
