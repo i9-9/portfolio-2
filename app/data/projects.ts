b9abc50427ef93b7af44a12a18781756c159109a
i9-9 <yvan.vrs@gmail.com>
2025-02-04 15:20:43 -0300

    better layout on projects - new images for all

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -3,27 +3,27 @@ export const projects = [
       title: "El Desenfreno",
       description:
         "Website for poetry publishing company from Buenos Aires, Argentina",
-      image: "/projects-eldesenfreno/eight.png",
+      image: "/cards/desenfreno.svg",
       anchor: "https://eldesenfreno.com",
     },
     {
       title: "Kostüme",
       description:
         "Website/E-Commerce showcasing their minimalist fashion collections and unique design approach.",
-      image: "/projects-landing/kostume/presentation.png",
+      image: "/cards/kostume.svg",
       anchor: "https://kostumeweb.net",
 
     },
     {
       title: "Newtro",
       description: "Website for web3 artist community",
-      image: "",
+      image: "/cards/newtro.svg",
       anchor: "https://newtro.xyz",
     },
     {
       title: "Sisterhood",
       description: "Website for Austrian enterprise that curates handicraft and design pieces.",
-      image: "",
+      image: "/cards/sisterhood.svg",
       anchor: "https://hello09386.wixstudio.io/my-site-3",
     },
   ];
