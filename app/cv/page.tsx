01027a890e01b3ebea06f9f936b89041ad3d94f9
i9-9 <yvan.vrs@gmail.com>
2025-02-07 18:59:46 -0300

    added cv

---
+++ app/cv/page.tsx
@@ -0,0 +1,12 @@
+export default function CVPage() {
+    return (
+      <div className="w-full h-screen">
+        <iframe
+          src="/CV_Ivan_Nevares.pdf"
+          className="w-full h-full"
+          style={{ border: "none" }}
+        />
+      </div>
+    );
+  }
+  
