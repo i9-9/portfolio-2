9b42926abd14ed8990fb1107a2ac11c10c0432da
i9-9 <yvan.vrs@gmail.com>
2025-02-08 18:54:01 -0300

    bilingual cv

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,8 +1,8 @@
 export default function Loading() {
     return <div className="flex items-center h-screen justify-center text-verde  text-6xl animate-pulse">
       Loading
-      <div className="flex ">
-        <div className="dot animate-dot bg-white "></div>
+      <div className="flex text-lima">
+        <div className="dot animate-dot bg-mid-gray "></div>
         <div className="dot animate-dot animation-delay-300 bg-verde"></div>
         <div className="dot animate-dot animation-delay-600 bg-verde"></div>
       </div>
