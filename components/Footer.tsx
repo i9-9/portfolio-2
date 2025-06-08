3c9ef3c3ad5d60c320d1a6009185f234f77668ee
i9-9 <yvan.vrs@gmail.com>
2025-06-08 17:47:03 -0300

    Fix typography consistency and improve splash animations

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='py-1 md:pb-2  px-4 z-10 w-full'>
-      <div className='mx-auto text-center flex items-center justify-between border-t-[1px] border-b-[1px] border-solid border-verde '>
+      <div className='mx-auto text-center flex items-center justify-between border-t-[1px] border-b-[1px] border-solid border-mid-gray/50 '>
         <h5 className='text-verde font-light text-sm'>☯ {new Date().getFullYear() }</h5>
         <Link href='/'>
           <h6 className='font-bold text-verde text-md'>I / N</h6>
