b98f2c49b16a3fd67371981ec97caa494957e5fb
i9-9 <yvan.vrs@gmail.com>
2025-06-28 11:03:45 -0300

    CV view, download - preview image project

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -4,18 +4,21 @@ export const projects = [
     name: "El Desenfreno",
     anchor: "https://eldesenfreno.com",
     image: "/projects-eldesenfreno/el_desenfreno_macbook_mockup.png",
+    previewImage: "/projects/eldesenfreno.png"
   },
   {
     id: 2,
     name: "Kostüme",
     anchor: "https://www.kostumeweb.net",
     image: "/projects-landing/kostume/jul:24/4 - Macbook mockup.png",
+    previewImage: "/projects/kostume.png"
   },
   {
     id: 3,
     name: "Vino Rodante",
     anchor: "https://vinorodante.com",
     image: "/projects-sofar/sofar-1.png",
+    previewImage: "/projects/vinorodante.png"
   },
 ];
   
